eca3f04edbfb087144ad2267f5af28fd9290c022
snordale <snordale@gmail.com>
2019-10-05 17:00:37 -0400

    5 models

--- loader.py
+++ loader.py
@@ -4,6 +4,10 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import StratifiedKFold, KFold, cross_val_score
 from sklearn.metrics import mean_squared_error
 from sklearn.naive_bayes import MultinomialNB, BernoulliNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
 
 df = pandas.read_csv('data/reddit_train.csv', header=0)
 X = df['comments'].to_numpy()
@@ -14,7 +18,7 @@ tfidf = TfidfVectorizer(input='content',
     lowercase=True,
     analyzer='word',
     stop_words='english',
-    ngram_range=(2,2),
+    ngram_range=(1,1),
     max_df=1.0,
     min_df=1,
     max_features=None,
@@ -23,9 +27,17 @@ tfidf = TfidfVectorizer(input='content',
 X = tfidf.fit_transform(X)
 
 k_fold = KFold(n_splits=5)
-mnb = MultinomialNB()
-for train, test in k_fold.split(X,Y):
-    mnb.fit(X[train], Y[train])
-    print("MNB score = ",   mnb.score(X[test], Y[test]))
+models = {
+    #'lr': LogisticRegression(),
+    #'mnb': MultinomialNB(),
+    #'decision_tree': DecisionTreeClassifier(),
+    #'svm': SVC(),
+    'random_forest': RandomForestClassifier(n_estimators=10)
+}
 
-print(X)
+
+
+for train, test in k_fold.split(X,Y):
+    for model_name, model in models.items():
+        model.fit(X[train], Y[train])
+        print("%s score = " % (model_name),   model.score(X[test], Y[test]))
