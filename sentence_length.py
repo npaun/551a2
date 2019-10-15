1572218dc68f99a04944f1f4c53d652496480c63
snordale <snordale@gmail.com>
2019-10-15 10:03:49 -0400

    sentence_length feature

---
+++ sentence_length.py
@@ -0,0 +1,14 @@
+import numpy as np
+import nltk
+
+def find_avg_sent_len(X):
+
+	avg_sentence_lengths = np.array([])
+
+	for x in X:
+		comment_length = len(x)
+		sentences = nltk.sent_tokenize(x)
+		avg_len = np.array([comment_length/(len(sentences))])
+		avg_sentence_lengths = np.append(avg_sentence_lengths, avg_len)
+
+	return avg_sentence_lengths
