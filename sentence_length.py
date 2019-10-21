bc0ee34f9a80bf23865e67e59ea645158661e6eb
snordale <snordale@gmail.com>
2019-10-20 22:31:40 -0400

    crap_injector

--- sentence_length.py
+++ sentence_length.py
@@ -2,18 +2,5 @@ import numpy as np
 import nltk
 
 def find_avg_sent_len(X):
-<<<<<<< HEAD
-
-	avg_sentence_lengths = np.array([])
-
-	for x in X:
-		comment_length = len(x)
-		sentences = nltk.sent_tokenize(x)
-		avg_len = np.array([comment_length/(len(sentences))])
-		avg_sentence_lengths = np.append(avg_sentence_lengths, avg_len)
-		
-	return avg_sentence_lengths
-=======
         res =  np.array([len(comment)/len(nltk.sent_tokenize(comment)) for comment in X])
         return res
->>>>>>> c171f4e119490e01b0e153b90c51cc2e36ffbab0
