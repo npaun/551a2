e6f45f11d4fd5cf012b2baabf6bf1b6141c4550a
Nicholas Paun <npaun@transit.app>
2019-10-20 21:27:23 -0400

    Model zoo

--- loader.py
+++ loader.py
@@ -2,6 +2,7 @@ import pandas
 import numpy as np
 
 
+from pprint import pprint
 import nltk
 from sklearn.feature_extraction.text import TfidfVectorizer, TfidfTransformer, CountVectorizer
 
@@ -39,6 +40,7 @@ import sentence_length
 df = pandas.read_csv('data/reddit_train.csv', header=0)
 print(df)
 comments = df['comments'].to_numpy()
+X = comments
 
 
 Y = df['subreddits'].to_numpy()
@@ -82,121 +84,119 @@ count_vector = CountVectorizer(
 
 
 pipeline = Pipeline([
-    ('cv', CountVectorizer),
-    ('tfidf', TfidfTransformer()),
+    ('cv', count_vector),
     ('clf', MultinomialNB()),
 ])
 
-# parameters = {
-#     'clf__alpha': (1, 0.1, 0.01, 0.001, 0.0001, 0.00001)
-# }
-# array = np.array([])
-# np.append(array, np.array([0]), axis=0)
-# print(array)
+class CrapInjector(object):
+    def __init__(self, comments):
+        self.comments = comments
+    def fit(self,X,Y=None):
+        return self
 
-avg_sentence_lengths = sentence_length.find_avg_sent_len(comments)
-csr_avg_sent_lens = scipy.sparse.csr_matrix(avg_sentence_lengths).transpose()
+    def transform(self, X, Y=None):
+        comments = self.comments
+        avg_sentence_lengths = sentence_length.find_avg_sent_len(comments)
+        csr_avg_sent_lens = scipy.sparse.csr_matrix(avg_sentence_lengths).transpose()
 
-comment_lengths = comment_length.find_comment_len(comments)
-csr_comment_lens = scipy.sparse.csr_matrix(comment_lengths).transpose()
+        comment_lengths = comment_length.find_comment_len(comments)
+        csr_comment_lens = scipy.sparse.csr_matrix(comment_lengths).transpose()
 
-nba_matches = nba_real.getMatches(comments)
-csr_nba_matches = scipy.sparse.csr_matrix(nba_matches).transpose()
+        nba_matches = nba_real.getMatches(comments)
+        csr_nba_matches = scipy.sparse.csr_matrix(nba_matches).transpose()
 
-nhl_matches = nhl_real.getMatches(comments)
-csr_nhl_matches = scipy.sparse.csr_matrix(nhl_matches).transpose()
+        nhl_matches = nhl_real.getMatches(comments)
+        csr_nhl_matches = scipy.sparse.csr_matrix(nhl_matches).transpose()
 
-lol_matches = lol_real.getMatches(comments)
-csr_lol_matches = scipy.sparse.csr_matrix(lol_matches).transpose()
+        lol_matches = lol_real.getMatches(comments)
+        csr_lol_matches = scipy.sparse.csr_matrix(lol_matches).transpose()
 
-trees_matches = trees_real.getMatches(comments)
-csr_trees_matches = scipy.sparse.csr_matrix(trees_matches).transpose()
+        trees_matches = trees_real.getMatches(comments)
+        csr_trees_matches = scipy.sparse.csr_matrix(trees_matches).transpose()
 
-soccer_matches = soccer.getMatches(comments)
-csr_soccer_matches = scipy.sparse.csr_matrix(soccer_matches).transpose()
+        soccer_matches = soccer.getMatches(comments)
+        csr_soccer_matches = scipy.sparse.csr_matrix(soccer_matches).transpose()
 
-overwatch_matches = overwatch.getMatches(comments)
-csr_overwatch_matches = scipy.sparse.csr_matrix(overwatch_matches).transpose()
+        overwatch_matches = overwatch.getMatches(comments)
+        csr_overwatch_matches = scipy.sparse.csr_matrix(overwatch_matches).transpose()
 
-nfl_matches = nfl.getMatches(comments)
-csr_nfl_matches = scipy.sparse.csr_matrix(nfl_matches).transpose()
+        nfl_matches = nfl.getMatches(comments)
+        csr_nfl_matches = scipy.sparse.csr_matrix(nfl_matches).transpose()
 
-got_matches = got.getMatches(comments)
-csr_got_matches = scipy.sparse.csr_matrix(got_matches).transpose()
+        got_matches = got.getMatches(comments)
+        csr_got_matches = scipy.sparse.csr_matrix(got_matches).transpose()
 
-baseball_matches = baseball.getMatches(comments)
-csr_baseball_matches = scipy.sparse.csr_matrix(baseball_matches).transpose()
+        baseball_matches = baseball.getMatches(comments)
+        csr_baseball_matches = scipy.sparse.csr_matrix(baseball_matches).transpose()
 
-canada_matches = canada.getMatches(comments)
-csr_canada_matches = scipy.sparse.csr_matrix(canada_matches).transpose()
+        canada_matches = canada.getMatches(comments)
+        csr_canada_matches = scipy.sparse.csr_matrix(canada_matches).transpose()
 
-csgo_matches = csgo.getMatches(comments)
-csr_csgo_matches = scipy.sparse.csr_matrix(csgo_matches).transpose()
+        csgo_matches = csgo.getMatches(comments)
+        csr_csgo_matches = scipy.sparse.csr_matrix(csgo_matches).transpose()
 
-anime_matches = anime.getMatches(comments)
-csr_anime_matches = scipy.sparse.csr_matrix(anime_matches).transpose()
+        anime_matches = anime.getMatches(comments)
+        csr_anime_matches = scipy.sparse.csr_matrix(anime_matches).transpose()
 
-music_matches = music.getMatches(comments)
-csr_music_matches = scipy.sparse.csr_matrix(music_matches).transpose()
+        music_matches = music.getMatches(comments)
+        csr_music_matches = scipy.sparse.csr_matrix(music_matches).transpose()
 
 
-#probs = sentence_length.find_avg_sent_len(comments)
-# print(avg_sentence_lengths)
+        #X = scipy.sparse.hstack((X, csr_avg_sent_lens), format='csr')
+        #X = scipy.sparse.hstack((X, csr_comment_lens), format='csr')
+        X = scipy.sparse.hstack((X, csr_nba_matches), format='csr')
+        X = scipy.sparse.hstack((X, csr_nhl_matches), format='csr')
+        X = scipy.sparse.hstack((X, csr_lol_matches), format='csr')
+        X = scipy.sparse.hstack((X, csr_trees_matches), format='csr')
+        X = scipy.sparse.hstack((X, csr_soccer_matches), format='csr')
+        X = scipy.sparse.hstack((X, csr_overwatch_matches), format='csr')
+        X = scipy.sparse.hstack((X, csr_nfl_matches), format='csr')
+        X = scipy.sparse.hstack((X, csr_got_matches), format='csr')
+        X = scipy.sparse.hstack((X, csr_baseball_matches), format='csr')
+        X = scipy.sparse.hstack((X, csr_canada_matches), format='csr')
+        X = scipy.sparse.hstack((X, csr_csgo_matches), format='csr')
+        X = scipy.sparse.hstack((X, csr_anime_matches), format='csr')
+        X = scipy.sparse.hstack((X, csr_music_matches), format='csr')
+        return X
 
-X = tfidf.fit_transform(comments)
-print(X.shape)
-#X = scipy.sparse.hstack((X, csr_avg_sent_lens), format='csr')
-#X = scipy.sparse.hstack((X, csr_comment_lens), format='csr')
-X = scipy.sparse.hstack((X, csr_nba_matches), format='csr')
-X = scipy.sparse.hstack((X, csr_nhl_matches), format='csr')
-X = scipy.sparse.hstack((X, csr_lol_matches), format='csr')
-X = scipy.sparse.hstack((X, csr_trees_matches), format='csr')
-X = scipy.sparse.hstack((X, csr_soccer_matches), format='csr')
-X = scipy.sparse.hstack((X, csr_overwatch_matches), format='csr')
-X = scipy.sparse.hstack((X, csr_nfl_matches), format='csr')
-X = scipy.sparse.hstack((X, csr_got_matches), format='csr')
-X = scipy.sparse.hstack((X, csr_baseball_matches), format='csr')
-X = scipy.sparse.hstack((X, csr_canada_matches), format='csr')
-X = scipy.sparse.hstack((X, csr_csgo_matches), format='csr')
-X = scipy.sparse.hstack((X, csr_anime_matches), format='csr')
-X = scipy.sparse.hstack((X, csr_music_matches), format='csr')
-print(X.shape)
+
 k_fold = KFold(n_splits=5)
 models = {
     'lr': LogisticRegression(),
     'mnb': MultinomialNB(),
-    'bnb': BernoulliNB(),
+    #'bnb': BernoulliNB(),
     #'decision_tree': DecisionTreeClassifier(),
     #'svm': SVC(),
     'random_forest': RandomForestClassifier(n_estimators=10)
 }
 
-# for train, test in k_fold.split(X,Y):
-#     for model_name, model in models.items():
-#         model.fit(X[train], Y[train])
-#         
+for model_name, model in models.items():
+    pipeline = Pipeline([
+        ('CountVectorizer', count_vector),
+        ('CrapInjector', CrapInjector(X)),
+        ('Classifier_' + model_name, model)])
+
+    parameters = {
+            'CountVectorizer__ngram_range': [(1,1),(2,2),(3,3)],
+            'CountVectorizer__binary': [True, False],
+            'CountVectorizer__stop_words': ['english', None],
+    }
 
 
-for train, test in k_fold.split(X,Y):
-    for model_name, model in models.items():
-
-        # print("Performing grid search...")
-        # print("pipeline:", [name for name, _ in pipeline.steps])
-        # print("parameters:")
-        # pprint(parameters)
-        t0 = time()
-
-        
-        # grid_search = GridSearchCV(pipeline, param_grid=parameters, cv=5, n_jobs=-1, verbose=1)
-        model.fit(X[train], Y[train])
-        print("%s score = " % (model_name), model.score(X[test], Y[test]))
+    
+    grid_search = GridSearchCV(pipeline, param_grid=parameters, cv=5, n_jobs=-1, verbose=1)
+    grid_search.fit(X, Y)
+    print("%s score = " % (model_name), grid_search.score(X, Y))
 
 
-        print("done in %0.3fs" % (time() - t0))
-        print()
+    try:
+        print("Best parameters set:")
+        best_parameters = grid_search.best_estimator_.get_params()
+        for param_name in sorted(parameters.keys()):
+             print("\t%s: %r" % (param_name, best_parameters[param_name]))
+    except:
+        pass
 
-        # print("Best score: %0.3f" % grid_search.best_score_)
-        # print("Best parameters set:")
-        # best_parameters = grid_search.best_estimator_.get_params()
-        # for param_name in sorted(parameters.keys()):
-        #     print("\t%s: %r" % (param_name, best_parameters[param_name]))
+
+    pprint(grid_search.cv_results_)
+
--- sentence_length.py
+++ sentence_length.py
@@ -2,19 +2,5 @@ import numpy as np
 import nltk
 
 def find_avg_sent_len(X):
-
-<<<<<<< HEAD
-	avg_sentence_lengths = np.array([])
-
-	for x in X:
-		comment_length = len(x)
-		sentences = nltk.sent_tokenize(x)
-		avg_len = np.array([comment_length/(len(sentences))])
-		avg_sentence_lengths = np.append(avg_sentence_lengths, avg_len)
-		
-	return avg_sentence_lengths
-=======
         res =  np.array([len(comment)/len(nltk.sent_tokenize(comment)) for comment in X])
-        print(res)
         return res
->>>>>>> 36cf0cbbe1591afa1465955970fc783c59ec7fc0
